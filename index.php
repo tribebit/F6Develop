fddf47b7a4e81489005e260ec0fa2341dc6349ec
tribebit <emanuele.casiraghi@fullsix.it>
2013-11-25 16:13:04 +0100

    secondo commit

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-    <title> prova</title>
+    <title>prova prova</title>
     <meta name="viewport" content="width=device-width">
     <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Lobster+Two" type="text/css">
     <link rel="icon" href="https://awsmedia.s3.amazonaws.com/favicon.ico" type="image/ico" >
